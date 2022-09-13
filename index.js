59fa302b9cc078cd418fc5d2f1302e8e171a9139
tb38r <tlawal86@hotmail.co.uk>
2022-09-13 21:48:09 +0100

    socket-io

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        //console.log('message: ' + msg);
+        io.emit('chat message', msg);
+
+      });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+  });
+  
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
